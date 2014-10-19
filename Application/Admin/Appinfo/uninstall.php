4839c1627c4c272f4a9b35d4b53389dd7f59089d
root <root@zuweie-HP-Pavilion-g4-Notebook-PC.(none)>
2014-10-19 16:54:16 +0800

    unstall.php

--- Application/Admin/Appinfo/uninstall.php
+++ Application/Admin/Appinfo/uninstall.php
@@ -4,7 +4,9 @@ if (!defined('THINK_PATH')) exit();
 $db_prefix = C('DB_PREFIX');
 
 $sql = array(
-	"DROP TABLE IF EXISTS `{$db_prefix}app`;"
+	"DROP TABLE IF EXISTS `{$db_prefix}app`;",
+	"DROP TABLE IF EXISTS `{$db_prefix}system_config`;",
+	"DROP TABLE IF EXISTS `{$db_prefix}system_data`;"
 );
 
 foreach ($sql as $v) {
--- Application/User/Appinfo/uninstall.php
+++ Application/User/Appinfo/uninstall.php
@@ -4,7 +4,11 @@ if (!defined('THINK_PATH')) exit();
 $db_prefix = C('DB_PREFIX');
 
 $sql = array(
-		"DROP TABLE IF EXISTS `{$db_prefix}user`;"
+		"DROP TABLE IF EXISTS `{$db_prefix}user`;",
+		"DROP TABLE IF EXISTS `{$db_prefix}user_privilege`;",
+		"DROP TABLE IF EXISTS `{$db_prefix}user_group`;",
+		"DROP TABLE IF EXISTS `{$db_prefix}group_privilege`;",
+		"DROP TABLE IF EXISTS `{$db_prefix}privilege`;"
 );
 
 foreach ($sql as $v) {
