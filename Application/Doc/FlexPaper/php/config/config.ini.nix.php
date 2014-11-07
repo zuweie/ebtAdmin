2c210828bcb5b7213eb0c395e9f42c79822f07cd
root <root@zuweie-HP-Pavilion-g4-Notebook-PC.(none)>
2014-11-07 15:04:57 +0800

    config.ini.nix.php

--- Application/Doc/FlexPaper/php/config/config.ini.nix.php
+++ Application/Doc/FlexPaper/php/config/config.ini.nix.php
@@ -1,2 +1,17 @@
 ; <?php exit; ?> DO NOT REMOVE THIS LINE
-{"allowcache":true,"splitmode":"0","path.pdf":"\/home\/zuweie\/workshop\/webapp\/Thinkphp_app\/Public\/Uploads\/doc\/","path.swf":"\/home\/zuweie\/workshop\/webapp\/Thinkphp_app\/Public\/Uploads\/doc\/","renderingorder.primary":null,"renderingorder.secondary":null,"cmd.conversion.singledoc":"\"\"pdf2swf\"\" \"{path.pdf}{pdffile}\" -o \"{path.swf}{pdffile}.swf\" -f -T 9 -t -s storeallcharacters -s linknameurl","cmd.conversion.splitpages":"\"\"pdf2swf\"\" \"{path.pdf}{pdffile}\" -o \"{path.swf}{pdffile}_%.swf\" -f -T 9 -t -s storeallcharacters -s linknameurl","cmd.conversion.renderpage":"\"\"swfrender\"\" \"{path.swf}{swffile}\" -p {page} -o \"{path.swf}{pdffile}_{page}.png\" -X 1024 -s keepaspectratio","cmd.conversion.rendersplitpage":"\"\"swfrender\"\" \"{path.swf}{swffile}\" -o \"{path.swf}{pdffile}_{page}.png\" -X 1024 -s keepaspectratio","cmd.conversion.jsonfile":"\"\"pdf2json\"\" \"{path.pdf}{pdffile}\" -enc UTF-8 -compress \"{path.swf}{pdffile}.js\"","cmd.conversion.splitjsonfile":"pdf2json \"{path.pdf}{pdffile}\" -enc UTF-8 -compress -split 10 \"{path.swf}{pdffile}_%.js\"","cmd.searching.extracttext":"\"\"swfstrings\"\" \"{swffile}\"","cmd.query.swfwidth":"swfdump {swffile} -X","cmd.query.swfheight":"swfdump \"{swffile}\" -Y","pdf2swf":true,"admin.username":"admin","admin.password":"1234","licensekey":""}
+{
+"allowcache":true,
+"splitmode":false,
+"path.pdf":"\/var\/www\/flexpaper\/pdf\/",
+"path.swf":"\/var\/www\/flexpaper\/docs\/",
+"renderingorder.primary":"flash",
+"renderingorder.secondary":"html",
+"cmd.conversion.singledoc":"pdf2swf \"{path.pdf}{pdffile}\" -o \"{path.swf}{pdffile}.swf\" -f -T 9 -t -s storeallcharacters -s linknameurl",
+"cmd.conversion.splitpages":"pdf2swf \"{path.pdf}{pdffile}\" -o \"{path.swf}{pdffile}_%.swf\" -f -T 9 -t -s storeallcharacters -s linknameurl",
+"cmd.conversion.renderpage":"swfrender \"{path.swf}{swffile}\" -p {page} -o \"{path.swf}{pdffile}_{page}.png\" -X 1024 -s keepaspectratio",
+"cmd.conversion.rendersplitpage":"swfrender \"{path.swf}{swffile}\" -o \"{path.swf}{pdffile}_{page}.png\" -X 1024 -s keepaspectratio",
+"cmd.conversion.jsonfile":"pdf2json \"{path.pdf}{pdffile}\" -enc UTF-8 -compress \"{path.swf}{pdffile}.js\"",
+"cmd.conversion.splitjsonfile":"pdf2json \"{path.pdf}{pdffile}\" -enc UTF-8 -compress -split 10 \"{path.swf}{pdffile}_%.js\"",
+"cmd.searching.extracttext":"swfstrings \"{swffile}\"","cmd.query.swfwidth":"swfdump {swffile} -X",
+"cmd.query.swfheight":"swfdump \"{swffile}\" -Y"
+}
